035941aabc58bf45e9f3959ce0c7c40e2b2b6ffb
Alezithop <alezithop@hotmail.com>
2022-03-24 18:22:09 -0600

    primer proyecto Vue generado por comandos

---
+++ practicas/VUE/vue-launchx/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Hola Vueplorers</h1>
+  <HelloWorld msg="Bienvenido to Your Vue.js App"/>
+</template>
+
+<script>
+/* todo el código de Vue */
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.titulo {
+  font-size: 50px;
+}
+</style>
